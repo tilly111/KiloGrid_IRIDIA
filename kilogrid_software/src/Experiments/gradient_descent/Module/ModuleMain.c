9a40d86ddc2e0291de1e5269e89569b6d1a7ca50
Antoine <antoine.sion@unamur.be>
2021-12-07 12:26:34 +0100

    broadcasting x y

--- kilogrid_software/src/Experiments/gradient_descent/Module/ModuleMain.c
+++ kilogrid_software/src/Experiments/gradient_descent/Module/ModuleMain.c
@@ -13,10 +13,23 @@
 volatile uint8_t cell_x[4] = {0, 0, 0, 0};
 volatile uint8_t cell_y[4] = {0, 0, 0, 0};
 volatile uint8_t cell_role[4] = {0, 0, 0, 0};
-
+volatile uint8_t cell_msg[4] = {0, 0, 0, 0};
 
 cell_num_t cell_id[4] = {CELL_00, CELL_01, CELL_02, CELL_03};
 
+IR_message_t IR_message_tx[4];
+IR_message_t IR_msg_rx;
+
+tracking_user_data_t tracking_data;
+
+void IR_rx(IR_message_t *m, cell_num_t c, distance_measurement_t *d, uint8_t CRC_error) {
+  if(!CRC_error && m->type == TRACKING){
+    CAN_message_t* msg = next_CAN_message();
+    if(msg != NULL) { // if the buffer is not full
+      serialize_tracking_message(msg, c, m->data);
+    }
+  }
+}
 
 void setup()
 {
@@ -40,6 +53,11 @@ void loop()
 {
 	for(int i = 0; i < 4; ++i)
 	{
+     IR_message_tx[i].type = 1;
+     IR_message_tx[i].data[0] = cell_x[i];
+     IR_message_tx[i].data[1] = cell_y[i];
+
+     set_IR_message(&IR_message_tx[i], i);
 
 		switch(cell_role[i])
 		{
