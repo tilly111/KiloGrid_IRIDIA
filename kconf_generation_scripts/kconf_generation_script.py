fdf7ef7576efbf2182c444c158c2f41737f0e142
Till.Aust <till.aust@student.uni-luebeck.de>
2021-12-07 11:22:45 +0100

    added kconf dile

--- kconf_generation_scripts/kconf_generation_script.py
+++ kconf_generation_scripts/kconf_generation_script.py
@@ -4,6 +4,7 @@ import numpy as np
 # Parameters to choose
 conf_file_name = 'test_config'
 wall_value = 42
+target_value = 10
 
 configuration_grid = np.zeros((20, 40))
 
@@ -16,6 +17,13 @@ def make_walls(grid):
 
 
 if __name__ == "__main__":
+    # setting up the configuration
+    # walls
+    #configuration_grid = make_walls(configuration_grid)
+    # setting the target
+    #configuration_grid[12:14, 12:14] = target_value
+
+    #print(configuration_grid)
 
     with open(conf_file_name + '.kconf', 'w') as f:
         # meta data TODO
@@ -25,16 +33,16 @@ if __name__ == "__main__":
         print('# Generation Time: ', dt_string, file=f)
         print('\n', file=f)
 
-        configuration_grid = make_walls(configuration_grid)
         # writing for each module
         for x in range(10):
             for y in range(20):
                 # print header
                 print('address', file=f)
-                print('module:', x, '-', y, '\n', file=f)
+                print('module:' + str(x) + '-' + str(y) + '\n', file=f)
                 print('data', file=f)
                 # fill with values ..
-                print('0x42', file=f)
+                for i in range(4):
+                    print(hex(i), file=f)
 
                 # needed for structure
                 print('', file=f)
