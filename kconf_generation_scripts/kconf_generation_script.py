d070720290e2c7054899eff0eca0c87eb58f2bc5
Till.Aust <till.aust@student.uni-luebeck.de>
2021-12-09 11:38:02 +0100

    updated script generation

--- kconf_generation_scripts/kconf_generation_script.py
+++ kconf_generation_scripts/kconf_generation_script.py
@@ -1,17 +1,13 @@
 from datetime import datetime
 import numpy as np
-import imageio
-import matplotlib.pyplot as plt
-#import visvis as vv
 import cv2 as cv
 
-# Parameters to choose
-conf_file_name = 'christmas_final'
-wall_value = 42
-target_value = 10
+# TODO Parameters to choose
+conf_file_name = 'christmas_final'  # name of the generated config file
+image_name = "pixil-frame-0-2.png"  # name of the image file
 
 configuration_grid = np.zeros((10, 20))
-
+wall_value = 42
 
 def make_walls(grid):
     grid[:, 0:1] = wall_value
@@ -20,50 +16,30 @@ def make_walls(grid):
     grid[-1:, :] = wall_value
     return grid
 
-def make_tree(grid):
-    begin = [4, 3, 2, 4, 3, 2, 4, 3, 2, 4, 3, 2, 4, 4, 4, 4, 4, 4]
-    end =   [6, 7, 8, 6, 7, 8, 6, 7, 8, 6, 7, 8, 6, 6, 6, 6, 6, 6]
-    for y in range(16):
-        if y < 12:
-            grid[begin[y]:end[y], 17-y] = np.ones((end[y]-begin[y],)) * 24
-        else:
-            grid[begin[y]:end[y], 17 - y] = np.ones((end[y] - begin[y],)) * 25
-
-    return grid
 
 if __name__ == "__main__":
     # setting up the configuration
-    # walls
+    # walls -> check if you need this configuration!!
     configuration_grid = make_walls(configuration_grid)
-    # setting the target
-    #configuration_grid[6:7, 6:7] = target_value
-    #configuration_grid = make_tree(configuration_grid)
-    #for y in range(20):
-    #    print(configuration_grid[:, y])
 
-    #print(configuration_grid_manual.shape)
-    #im = imageio.imread('pixil-frame-0.png')
-    im = cv.imread("pixil-frame-0-2.png")
+    im = cv.imread(image_name)
+
     gray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
     gray = gray.transpose()
     gray = np.fliplr(gray)
     gray = np.rot90(gray, 2)
+
+    # TODO change values according to your image
     gray[gray == 99] = 3
     gray[gray == 87] = 1
     gray[gray == 135] = 2
     gray[gray == 94] = 4
     gray[gray == 224] = 5
-    #im = plt.imread('pixil-frame-0.png')
     print(gray)
 
     print(gray.shape)
-    #matrix = np.array(im)
-    #print(matrix.shape)
-
-
 
     with open(conf_file_name + '.kconf', 'w') as f:
-        # meta data TODO
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         print('# Experiment: ', conf_file_name, file=f)
@@ -85,6 +61,5 @@ if __name__ == "__main__":
                 print(hex(gray[x * 2 + 1, y * 2 + 1]), file=f)
                 print(hex(gray[x * 2, y * 2]), file=f)
                 print(hex(gray[x * 2 + 1, y * 2]), file=f)
-
                 # needed for structure
                 print('', file=f)
